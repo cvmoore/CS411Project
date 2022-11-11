f97392f521bd560502116feb2e1262f26890d725
Carter Vande Moore <72159057+cvmoore@users.noreply.github.com>
2022-11-11 10:29:52 -0500

    Added handleSubmit and constants for form

--- code/Frontend/frontend/src/App.js
+++ code/Frontend/frontend/src/App.js
@@ -1,15 +1,44 @@
 import React from 'react';
+import {useState} from 'react'
+import {setMessage} from 'react'
 import './App.css';
 
 function App() {
+  const [keyword, setKeyword] = useState("");
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("https://httpbin.org/post", {
+        method: "POST",
+        body: JSON.stringify({
+          keyword: keyword,
+        }),
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setMessage("User created successfully");
+      } else {
+        setMessage("Some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return(
     <div className="wrapper">
       <h1>Input Search Keyword</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
       <fieldset>
          <label>
            <p>Keyword</p>
-           <input keyword="keyword" />
+           <input
+           type="text"
+           value={keyword}
+           placeholder="Keyword"
+           onChange={(e) => setKeyword(e.target.value)}
+         />
          </label>
        </fieldset>
        <button type="submit">Submit</button>
